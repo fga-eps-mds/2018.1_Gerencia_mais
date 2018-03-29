dadf7c664dc100fc0649915cf12cf03359f8f38f
victorhdcoelho <victorhdcoelho@gmail.com>
2018-03-29 19:58:17 -0300

    Signed-off-by: victorhdcoelho <victorhdcoelho@gmail.com> Signed-off-by: adriellyrodrigues <drykaa1@gmail.com> Create model of doctor's hours

--- leito_hospitalar/leito_hospitalar/settings.py
+++ leito_hospitalar/leito_hospitalar/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webpack_loader',
+    'doctorhour',
 ]
 
 MIDDLEWARE = [
--- leito_hospitalar/leito_hospitalar/urls.py
+++ leito_hospitalar/leito_hospitalar/urls.py
@@ -20,6 +20,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', TemplateView.as_view(template_name="index.html")),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
 ]
-
