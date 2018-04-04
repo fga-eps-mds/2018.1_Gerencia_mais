a7a4b3b958fd7940aabb2d367db0f2e89688e1fa
Eduardo Júnio <eduardojvr10@gmail.com>
2018-04-04 00:51:52 -0300

    Rebuild project for deploy

---
+++ src/pages/ScheduleTable.jsx
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import NavBar from '../components/NavBar';
+import {Table,Button} from 'react-bootstrap';
+import Popup from "reactjs-popup";
+import "../css/popup.css";
+import "../css/bootstrap.min.css";
+
+class GridCell extends Component {
+  constructor(props){
+    super();
+    this.state = {"line":props.line,"column":props.column};
+  }
+
+    resolveButton(line,column){
+      /*
+        change this url to project API's url.
+        line == periods
+        column == days of the week
+      */
+      fetch("https://randomuser.me/api/?results=50&nat=us,dk,fr,br")
+      .then(response => response.json())
+      .then(parsedJSON =>parsedJSON.results)
+      .then(contacts => this.setState({
+        contacts
+      }))
+      .catch(error => console.log("error to get data " + error));
+    };
+
+  render(){
+    return(
+      <td onClick = {()=> this.resolveButton(this.props.line,this.props.column)}>
+      <Popup
+      trigger={<button className="btn btn-outline-secondary">Servidores</button>}
+      modal
+      closeOnDocumentClick
+      >
+      <div className="popupShape">
+        <div className="pre-scrollable">
+          {
+            this.state.contacts != null ? this.state.contacts.map(user =>
+              <div><span>{user.name.first + " " + user.name.last}</span><br></br></div>
+            )
+            : null
+          }
+        </div>
+      </div>
+      </Popup>
+      </td>
+    )
+  }
+}
+
+export default class ScheduleTable extends Component {
+    constructor(){
+      super();
+      this.state={"popup":0};
+    }
+
+
+    TableList(number){
+      var lists=[];
+      var periods = ["manhã","tarde","noite"];
+      for(var cont = 0;cont <= number; cont++){
+        lists.push(
+        <tr>
+        <td><h3>{periods[cont]}</h3></td>
+        <GridCell line={cont} column={0}></GridCell>
+        <GridCell line={cont} column={1}></GridCell>
+        <GridCell line={cont} column={2}></GridCell>
+        <GridCell line={cont} column={3}></GridCell>
+        <GridCell line={cont} column={4}></GridCell>
+        <GridCell line={cont} column={5}></GridCell>
+        <GridCell line={cont} column={6}></GridCell>
+        </tr>
+        )
+      }
+    return lists;
+    }
+
+    render() {
+	return (
+	    <div>
+	      <NavBar></NavBar>
+	      <h1 style={{marginTop:"70px"}}>Quadro de Horários</h1>
+        <Table striped bordered condensed hover>
+         <thead>
+     <tr>
+      <th>Horário</th>
+       <th>Domingo</th>
+       <th>Segunda</th>
+       <th>Terça</th>
+       <th>Quarta</th>
+       <th>Quinta</th>
+       <th>Sexta</th>
+       <th>Sábado</th>
+     </tr>
+         </thead>
+         <tbody>
+     {this.TableList(2)}
+
+         </tbody>
+       </Table>
+
+	    </div>
+	);
+    }
+}
