0d4db439d79a912e414d7d34085aaf0c4c075d19
Eduardo Júnio <eduardojvr10@gmail.com>
2018-04-07 18:33:50 -0300

    Fix bug redirect

--- leito_hospitalar/settings.py
+++ leito_hospitalar/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'leito_hospitalar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "build"), ],
+        'DIRS': [os.path.join(BASE_DIR, "build"),os.path.join(BASE_DIR, "templates"), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {Route, Switch, BrowserRouter} from 'react-router-dom';
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
 import Dashboard from "./pages/Dashboard";
+import DoctorForm from "./pages/DoctorForm";
+import DoctorStatus from "./pages/DoctorStatus";
 
 class App extends Component {
   render() {
@@ -11,6 +13,8 @@ class App extends Component {
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/dashboard" component={Dashboard} />
+      <Route exact path="/DoctorForm" component={DoctorForm} />
+      <Route exact path="/DoctorStatus" component={DoctorStatus} />
       <Route component={NotFound} />
     </Switch>
     </BrowserRouter>
