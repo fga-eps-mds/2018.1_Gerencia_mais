486ff42e2eab35b09a3f6de9c4e977e5092db5d9
victorhdcoelho <victorhdcoelho@gmail.com>
2018-05-04 22:54:24 -0300

    Fix code bugs in codeclimate

--- doctor/views.py
+++ doctor/views.py
@@ -7,7 +7,6 @@ from doctor.serializer import (
 
 from rest_framework import generics
 from rest_framework.permissions import(
-    IsAdminUser,
     AllowAny,
 )
 
--- leito_hospitalar/settings.py
+++ leito_hospitalar/settings.py
@@ -52,7 +52,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
-    'DEFAULT_FILTER_BACKENDS': 
+    'DEFAULT_FILTER_BACKENDS':
     ['django_filters.rest_framework.DjangoFilterBackend',]
 }
 
