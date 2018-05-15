8847ddfc4c98cb58d7d1c1d5168e8aebddf73f19
victorhdcoelho <victorhdcoelho@gmail.com>
2018-05-15 10:55:59 -0300

    Add postgress

--- leito_hospitalar/settings.py
+++ leito_hospitalar/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'administrator',
     'doctor',
     'django_filters',
+    'subtitle',
 ]
 
 REST_FRAMEWORK = {
@@ -98,14 +99,23 @@ WSGI_APPLICATION = 'leito_hospitalar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+# #
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
---
+++ subtitle/views.py
@@ -0,0 +1,35 @@
+from subtitle.models import Subtitles
+
+from subtitle.serializer import (
+    SubtitlesSerializer,
+    SubtitlesListSerializer,
+    SubtitlesCreateUpdateSerializer
+)
+
+from rest_framework import generics
+
+from rest_framework.permissions import (
+    AllowAny,
+)
+
+class ListSubtitles(generics.ListCreateAPIView):
+    permission_classes = [AllowAny]
+    queryset = Subtitles.objects.all()
+    serializer_class = SubtitlesSerializer
+
+class CreateSubtitles(generics.CreateAPIView):
+    permission_classes = [AllowAny]
+    queryset = Subtitles.objects.all()
+    serializer_class = SubtitlesCreateUpdateSerializer
+
+class ListDetailSubtitles(generics.RetrieveAPIView):
+    permission_classes = [AllowAny]
+    queryset = Subtitles.objects.all()
+    serializer_class = SubtitlesSerializer
+    lookup_field = 'code'
+
+class UpdateSubtitlesAPI(generics.RetrieveUpdateAPIView):
+    permission_classes = [AllowAny]
+    queryset = Subtitles.objects.all()
+    serializer_class = SubtitlesCreateUpdateSerializer
+    lookup_field = 'code'
