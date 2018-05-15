f3771c195ae45fbf9511037fa8f0b9c175d32188
victorhdcoelho <victorhdcoelho@gmail.com>
2018-05-15 10:56:56 -0300

    Add postgress

--- doctor/models.py
+++ doctor/models.py
@@ -6,6 +6,7 @@ class Doctors(models.Model):
     status = models.NullBooleanField()
     registration = models. CharField(max_length=50, unique=True)
     CPF = models.CharField(max_length=100, unique= True)
+    category = models.CharField(max_length=50)
 
     def __str__(self):
         return ugettext('%(name)s: %(registration)s') % {
