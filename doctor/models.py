0733cd6f092284dbcb9057fa928f7216674c9e0f
victorhdcoelho <victorhdcoelho@gmail.com>
2018-05-17 20:32:44 -0300

    Fix bugs in doctor app

--- doctor/models.py
+++ doctor/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext
 
-//adicionado a categoria nas models 
+#adicionado a categoria nas models 
 
 class Doctors(models.Model):
     name = models.CharField(max_length=255)
