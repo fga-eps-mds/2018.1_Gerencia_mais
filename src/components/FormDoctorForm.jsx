e59baca3c7133abbe157bc66e1772b59627f6bad
Guilherme-Aguiar <gui9627oli@gmail.com>
2018-06-10 21:07:34 -0300

    Refactoring code

--- src/components/FormDoctorForm.jsx
+++ src/components/FormDoctorForm.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-//import {Link} from 'react-router-dom';
+
 import '../css/bootstrap.css';
 import '../css/DoctorForm.css';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
-// import Select from 'react-select';
+
 import SideBar from '../components/SideBar';
 
 
 var date = new Date().toISOString();
-// status = True, title= 'Mauricio', start='2018-04-17T17:25:32Z', end='2018-04-20T17:25:34Z', calendar = calendar
+
 export default class FormDoctorForm extends Component {
   constructor(props) {
     super(props);
